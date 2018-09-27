8df32d8c85a235062790d1651f751084c20ffc12
Kevin Trebossen <Kevin@macbook-pro-de-kevin.local>
2018-09-27 13:33:15 +0200

    Initial Commit

---
+++ Betic/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Betic
+//
+//  Created by Kevin Trebossen on 27/09/2018.
+//  Copyright © 2018 Kevin Trebossen. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Betic/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Betic
+//
+//  Created by Kevin Trebossen on 27/09/2018.
+//  Copyright © 2018 Kevin Trebossen. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
